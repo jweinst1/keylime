0c6be44a9bf2b744fe65a3753cad9708015699a4
Joshua Weinstein <jweinstein@splunk.com>
2018-01-30 14:43:16 -0800

    lib works for header

---
+++ src/keylimelib.h
@@ -0,0 +1,7 @@
+#ifndef KEY_LIME_LIB_H
+#define KEY_LIME_LIB_H
+// main header for key lime static library
+
+#include "vm.h"
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "keylimelib.h"
 
 int main(int argc, char const *argv[]) {
-        puts("Hello !");
+        printf("result is %d", chAdd('e', 'f'));
         return 0;
 }
