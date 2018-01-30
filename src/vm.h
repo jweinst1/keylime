15bd4a12a4d22a56a4090fd8d9403160ff54bb96
Joshua Weinstein <jweinstein@splunk.com>
2018-01-30 14:56:15 -0800

    created register and type

--- src/vm.h
+++ src/vm.h
@@ -1,7 +1,26 @@
 #ifndef VM_HEADER
 #define VM_HEADER
 
+#include <stdlib.h>
+
+#define KEYLIME_REG 5
+
+typedef enum
+{
+        KeyLimeType_Int,
+        KeyLimeType_Bool,
+        KeyLimeType_Str
+} KeyLimeType;
+
+typedef struct
+{
+        void* item;
+        KeyLimeType type;
+} KeyLimeRegister;
+
 char chAdd(char a, char b);
+
+
 
 
 #endif
