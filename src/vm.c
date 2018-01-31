c19c8712eadd738d8370dccd90c0c57190412928
Joshua Weinstein <jweinstein@splunk.com>
2018-01-31 11:39:13 -0800

    new commands

--- src/vm.c
+++ src/vm.c
@@ -2,7 +2,25 @@
 
 static KeyLimeRegister REGISTERS[KEYLIME_REG_C];
 
-KeyLimeType getRegType(int index)
+KeyLimeType KeyLime_getRegType(int index)
 {
-        return REGISTERS[index].type;
+        return REGISTERS[index % KEYLIME_REG_C].type;
+}
+
+void KeyLime_setReg(int index, void* ptr)
+{
+        REGISTERS[index % KEYLIME_REG_C].item = ptr;
+}
+
+void* KeyLime_getReg(int index)
+{
+        return REGISTERS[index % KEYLIME_REG_C].item;
+}
+
+void KeyLime_newInt(int index, int value)
+{
+        free(REGISTERS[index % KEYLIME_REG_C].item);
+        REGISTERS[index % KEYLIME_REG_C].type = KeyLimeType_Int;
+        REGISTERS[index % KEYLIME_REG_C].item = malloc(sizeof(int));
+        *(int*)(REGISTERS[index % KEYLIME_REG_C].item) = value;
 }
--- src/vm.h
+++ src/vm.h
@@ -7,6 +7,7 @@
 
 typedef enum
 {
+        KeyLimeType_Null,
         KeyLimeType_Int,
         KeyLimeType_Bool,
         KeyLimeType_Str
@@ -18,7 +19,15 @@ typedef struct
         KeyLimeType type;
 } KeyLimeRegister;
 
-KeyLimeType getRegType(int index);
+KeyLimeType KeyLime_getRegType(int index);
+
+void KeyLime_setReg(int index, void* ptr);
+
+// Gets void pointer of register.
+void* KeyLime_getReg(int index);
+
+// Creates a new int in some register, frees old value.
+void KeyLime_newInt(int index, int value);
 
 
 
